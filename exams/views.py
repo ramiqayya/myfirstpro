e660cc59ef42b1115e25d2431fb75159cdd1afa1
ramiqayya <rami.qayya@gmail.com>
2023-06-13 19:50:19 +0300

    make some changes

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("<button>Hello world</button>")
+
+
+def takeha(request, andy):
+    return HttpResponse(f"takfeen ya {andy.capitalize()}")
