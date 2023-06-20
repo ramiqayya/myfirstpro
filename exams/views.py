f0327e3ec010efb89eb9a0f5c4bf60d385b4f8eb
ramiqayya <rami.qayya@gmail.com>
2023-06-20 14:42:10 +0300

    modify navbar

--- exams/views.py
+++ exams/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+
 # Create your views here.
 from .models import User
 from django.db import IntegrityError
 
 
+@login_required(login_url='/login')
 def index(request):
     return render(request, "exams/home.html")
 
